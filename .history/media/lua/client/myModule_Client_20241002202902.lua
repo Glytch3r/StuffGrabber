a9aa94bdc667a9efe142043ddc5f41200826f905
Glytch3r <glytch3r.arts@gmail.com>
2024-10-06 06:33:54 +0800

    init

---
+++ .history/media/lua/client/myModule_Client_20241002202902.lua
@@ -0,0 +1,173 @@
+--[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                        				 Custom  PZ  Mod  Developer  for  Hire													  |
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                       	Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/							          |
+|                       		                                    														 	  |
+|                       	Discord:    Glytch3r#1337 / glytch3r															      |
+|                       		                                    														 	  |
+|                       	Support:    https://ko-fi.com/glytch3r														    	  |
+|_______________________________________________________________________________________________________________________________-]]
+--[[_____________________________________________________________________________________________________________________________
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
+
+
+
+myModule = myModule or {}
+
+local Commands = {};
+Commands.myModule = {};
+
+-----------------------            ---------------------------
+
+myModule.outfit1 = 'fit1'
+myModule.outfit2 = 'fit2'
+
+
+function myModule.isDay()
+	return forageSystem.getTimeOfDay() == 'isDay'
+end
+
+function myModule.isNight()
+	return forageSystem.getTimeOfDay() == 'isNight'
+end
+
+function myModule.getDayTime()
+	if myModule.isNight() then return 'Night' end
+	if myModule.isDay() then return 'Day' end
+	return ''
+end
+
+
+function myModule.ismyModule(zed)
+	if not zed then return false end
+	local fit = tostring(zed:getOutfitName())
+	if fit == myModule.outfit1 then return true end
+	if fit == myModule.outfit2 then return true end
+	return false
+end
+
+
+function myModule.setOutfit(zed, outfitNum)
+    local wearNum = myModule.getOutfitNum(zed)
+    if wearNum ~= outfitNum then
+        if outfitNum == 1 then
+            zed:dressInNamedOutfit(myModule.outfit1)  -- myModule_1 skin
+        elseif outfitNum == 2 then
+            zed:dressInNamedOutfit(myModule.outfit2)  -- alt outfit
+        end
+        zed:resetModelNextFrame()
+        --myModule.setWalk(zed)
+       -- zed:DoZombieStats()
+    end
+end
+
+-----------------------            ---------------------------
+function myModule.findzedID(int)
+	local zombies = getCell():getObjectList()
+	for i=zombies:size(),1,-1 do
+		local zed = zombies:get(i-1)
+		if instanceof(zed, "IsoZombie") then
+			local zedID=zed:getOnlineID()
+			if zedID and zedID == int then return zed end
+		end
+	end
+	return nil
+end
+
+
+-----------------------            ---------------------------
+function myModule.coreFunc(zed)
+    if not zed then return end
+
+    local zedVar = zed:getVariableBoolean('ismyModule')
+    if myModule.ismyModule(zed) and not zedVar then
+        zed:setVariable('ismyModule', 'true')
+        if isClient() then
+            sendClientCommand('myModule', 'ismyModule', {ismyModule = true, zedID = zed:getOnlineID()})
+        end
+    elseif not myModule.ismyModule(zed) and zedVar then
+        zed:setVariable('ismyModule', 'false')
+        if isClient() then
+            sendClientCommand('myModule', 'ismyModule', {ismyModule = false, zedID = zed:getOnlineID()})
+        end
+    end
+end
+
+Events.OnZombieUpdate.Remove(myModule.coreFunc)
+Events.OnZombieUpdate.Add(myModule.coreFunc)
+-----------------------            ---------------------------
+Commands.myModule.isWearingmyModule = function(args)
+    local source = getPlayer();
+    local player = getPlayerByOnlineID(args.id)
+    if source ~= player then
+        if args.isWearingmyModule then
+			if not player:isHideWeaponModel() then player:setHideWeaponModel(true) end
+            if player:getVariableBoolean('isWearingmyModule') == false then
+                player:setVariable('isWearingmyModule', true);
+            end
+        else
+			if player:isHideWeaponModel() then player:setHideWeaponModel(false) end
+            if player:getVariableBoolean('isWearingmyModule')  then
+                player:setVariable('isWearingmyModule', false);
+            end
+        end
+    end
+end
+
+
+
+-----------------------            ---------------------------
+
+
+Commands.myModule.ismyModule = function(args)
+    local source = getPlayer();
+    local player = getPlayerByOnlineID(args.id)
+    local zedID = args.zedID
+    if type(zedID) == 'string' then zedID = tonumber(zedID) end
+    local zed = myModule.findzedID(zedID)
+    if zed ~= nil then
+		if source ~= player then
+			if args.ismyModule then
+				zed:setVariable('ismyModule', 'true');
+			else
+				zed:setVariable('ismyModule', 'false');
+			end
+		end
+    end
+end
+Commands.myModule.knockDownZed = function(args)
+    local source = getPlayer();
+    local player = getPlayerByOnlineID(args.id)
+    local zedID = args.zedID
+    if type(zedID) == 'string' then zedID = tonumber(zedID) end
+    local zed = myModule.findzedID(zedID)
+	if source ~= player then
+		if zed ~= nil then
+			zed:setKnockedDown(true)
+		end
+	end
+end
+
+
+
+
+Events.OnServerCommand.Add(function(module, command, args)
+	if Commands[module] and Commands[module][command] then
+		Commands[module][command](args)
+	end
+end)
---
+++ .history/media/lua/client/myModule_Client_20241006024547.lua
@@ -0,0 +1,88 @@
+--[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                        				 Custom  PZ  Mod  Developer  for  Hire													  |
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                       	Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/							          |
+|                       		                                    														 	  |
+|                       	Discord:    Glytch3r#1337 / glytch3r															      |
+|                       		                                    														 	  |
+|                       	Support:    https://ko-fi.com/glytch3r														    	  |
+|_______________________________________________________________________________________________________________________________-]]
+--[[_____________________________________________________________________________________________________________________________
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
+
+
+
+StuffGrabber = StuffGrabber or {}
+
+
+-----------------------            ---------------------------
+--[[
+if instanceof(item, "IsoWorldInventoryObject") then
+
+end
+ ]]
+--[[
+local stuff =
+for i,_ in pairs(stuff) do
+    local item = i
+     item = pl:getInventory():AddItem(body:getItem())
+    body:getSquare():removeCorpse(body, false)
+    local dropX,dropY,dropZ = ISInventoryTransferAction.GetDropItemOffset(pl, pSq, bodyItem)
+    pl:getCurrentSquare():AddWorldInventoryItem(bodyItem, dropX, dropY, dropZ)
+    pl:getInventory():Remove(bodyItem)
+end
+ISInventoryPage.renderDirty = true
+
+
+
+function StuffGrabber.getStuffList()
+    return SandboxVars.StuffGrabber.StuffList or  "Base.Katana;Base.Apple;Base.Log"
+end
+
+]]
+
+function StuffGrabber.parseList()
+    local items = {}
+    local strList = StuffGrabber.getStuffList()
+    for item in string.gmatch(strList, "[^;]+") do
+        table.insert(items, item)
+    end
+    return items
+end
+
+function StuffGrabber.context(player, context, worldobjects, test)
+	local pl = getSpecificPlayer(player)
+	local sq = clickedSquare
+	local targ = clickedPlayer
+	if sq then
+		local Main = context:addOptionOnTop("StuffGrabber: ")
+		Main.iconTexture = getTexture("media/ui/chop_tree.png")
+		local opt = ISContextMenu:getNew(context)
+		context:addSubMenu(Main, opt)
+		local optTip = opt:addOption('stuff', worldobjects, function()
+			context:hideAndChildren()
+		end)
+		local tip = ISWorldObjectContextMenu.addToolTip()
+		tip.description = ""
+		optTip.toolTip:setName(Main:getName())
+		optTip.toolTip = tip
+		optTip.notAvailable = true
+	end
+end
+Events.OnFillWorldObjectContextMenu.Remove(StuffGrabber.context)
+Events.OnFillWorldObjectContextMenu.Add(StuffGrabber.context)
